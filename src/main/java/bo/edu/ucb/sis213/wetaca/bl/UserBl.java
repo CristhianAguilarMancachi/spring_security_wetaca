60cf38335f9d36c15ef912a08106e21f0d401fdf
jose marcos luna <marcoslunahbl@gmail.com>
2022-12-03 21:07:49 -0400

    actualizacion en userBl

--- src/main/java/bo/edu/ucb/sis213/wetaca/bl/UserBl.java
+++ src/main/java/bo/edu/ucb/sis213/wetaca/bl/UserBl.java
@@ -2,7 +2,8 @@ package bo.edu.ucb.sis213.wetaca.bl;
 
 import at.favre.lib.crypto.bcrypt.BCrypt;
 import bo.edu.ucb.sis213.wetaca.dao.UserDao;
-import bo.edu.ucb.sis213.wetaca.dto.CreateUserDto;
+import bo.edu.ucb.sis213.wetaca.dao.UserPersonDao;
+import bo.edu.ucb.sis213.wetaca.dto.CrearUsuarioDto;
 import bo.edu.ucb.sis213.wetaca.entity.Wtc_usuario;
 import bo.edu.ucb.sis213.wetaca.entity.UserPerson;
 import org.springframework.stereotype.Service;
@@ -17,12 +18,12 @@ public class UserBl {
         this.userPersonDao = userPersonDao;
     }
 
-    public void createUser(CreateUserDto createUserDto) {
+    public void createUser(CrearUsuarioDto createUserDto) {
         Wtc_usuario mrUser = new Wtc_usuario();
-        mrUser.setCi_wtc_usuario(createUserDto.getUsername());
+        mrUser.setCi_wtc_usuario(createUserDto.getNombreUsuario_wtc_usuario());
         // Encrypt secret with BCrypt
         String secret = BCrypt.withDefaults().hashToString(12,
-                createUserDto.getSecret().toCharArray());
+                createUserDto.getContrasena_wtc_usuario().toCharArray());
         mrUser.setNombre_wtc_usuario(secret);
         this.mrUserDao.createUser(mrUser);
     }
